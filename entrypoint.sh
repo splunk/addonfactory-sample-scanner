8ae6cb9fb006462d50b6718f4929ba6a151e4a9d
Addon Factory template <addonfactory@splunk.com>
2021-05-25 15:18:34 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-go-earlybird  -show-solutions -suppress -config=/.go-earlybird/  -ignorefile=/.ge_ignore -path=$(pwd)/${INPUT_SCANDIR} ${INPUT_ARGS}
+go-earlybird  -show-solutions -suppress -config=/.go-earlybird/  -ignorefile=/.ge_ignore -path=$GITHUB_WORKSPACE/${INPUT_SCANDIR} ${INPUT_ARGS}

2d8fedfba093c1f48fd779bc1ef80036354ae978
Addon Factory template <addonfactory@splunk.com>
2021-05-25 16:52:27 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 echo "git submodules"
-git submodule init
+pushd $GITHUB_ACTION_PATH
 git submodule update --init --recursive
-env
 ls -l
 pushd earlybird
 ./build.sh
 ./install.sh
+popd
 popd

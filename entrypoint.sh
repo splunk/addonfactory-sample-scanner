9484eb5955dbf73c05a47438a5c2af9bdc0a092b
Addon Factory template <addonfactory@splunk.com>
2021-05-26 12:38:45 -0400

    fix type

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,15 +8,14 @@ echo ::group::reviewdog_output
 
 go-earlybird  -show-solutions -suppress -config=/.go-earlybird/  -ignorefile=/.ge_ignore \
     -path=/github/workspace/${INPUT_WORKDIR} -format=json ${INPUT_ARGS} \
-    | python3 /annotate.py 
-    # \
-    # | reviewdog -f=rdjson  \
-    #   -name="linter-name (addonfactory-sample-scanner)" \
-    #   -reporter="${INPUT_REPORTER:-github-pr-check}" \
-    #   -filter-mode="${INPUT_FILTER_MODE}" \
-    #   -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
-    #   -level="${INPUT_LEVEL}" \
-    #   ${INPUT_REVIEWDOG_FLAGS} ; exitCode=$?
+    | python3 /annotate.py \
+    | reviewdog -f=rdjson  \
+      -name="linter-name (addonfactory-sample-scanner)" \
+      -reporter="${INPUT_REPORTER:-github-check}" \
+      -filter-mode="${INPUT_FILTER_MODE}" \
+      -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
+      -level="${INPUT_LEVEL}" \
+      ${INPUT_REVIEWDOG_FLAGS} ; exitCode=$?
 
 echo "::endgroup::"
 
